ab7232beda17901b2915f51632e362967ae9dcfa
boconnell2 <bmoconnell15@gmail.com>
2021-01-04 16:41:46 -0500

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-console.log('testing')
+/*********Dom Elements*********/
+const display = document.querySelector("#container")
+let currentUser = 
 
-// testing brendan branch 
+/*********Render Functions*********/
+function renderBook(bookObj){
+    display.innerHTML += `
+     <h2>${bookObj.title}</h2>
+     <h3> ${bookObj.author}</h3>
+     <p> ${bookObj.year} </p>
+
+    `
+}
+
+/*********Fetch Requests*********/
+function getBooks() {
+    fetch('http://localhost:3000/api/v1/books')
+    .then(res => res.json())
+    .then(bookArray => {
+        bookArray.forEach((book) => {
+            renderBook(book)
+        })
+    })
+}
+
+/*********Event Listeners*********/
+
+
+/*********Initialize*********/
+getBooks()

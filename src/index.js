3f1954fad47e9c6179200e6726f5d789e1c1787b
boconnell2 <bmoconnell15@gmail.com>
2021-01-06 14:33:53 -0500

    mid day

--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
 /*********Dom Elements*********/
+
+// Home Page Elements
+const homePage = document.querySelector("#home-page")
+const homePageHeaderButton = document.querySelector(".header-title")
 const bookDisplayHome = document.querySelector("#container")
 const userDisplayHome = document.querySelector(".user-home")
+const 
+
 const userBookshelf = document.querySelector("#bookshelf-children")
 const userBorrowings = document.querySelector("#borrowings-children")
-const bookShowContainer = document.querySelector("#book-show")
+const bookInfoContainer = document.querySelector(".book-info")
 const bookShowCopies = document.querySelector(".copies")
+const myLibraryButton = document.querySelector(".profile-menu")
+const libraryPage = document.querySelector("#library-page")
 let currentUser = 'bert.hilll'
 let currentUserId = '2'
 let currentUserIdInt = 2
 
 /*********Render Functions*********/
 function renderBook(bookObj){
-    bookDisplayHome.innerHTML += `
-    <div class="book-card"> 
-     <h4>${bookObj.title}</h4>
     
+    bookDisplayHome.innerHTML += `
+    <div class="book-card" id=${bookObj.id}> 
+     <h4>${bookObj.title}</h4>
      <h6> ${bookObj.author}</h6>
-     
      <p> ${bookObj.year} </p>
     </div>
+    `
+
+}
+/********** Clear Page Function ***********/
+function clearHomePage() {
+    h3.textContent = ''
+    bookDisplayHome.innerHTML = ''
+    ...
+}
+
+function renderHomePage() {
+    
+
+    libraryPage.innerHTML = ""
+    homePage.innerHTML += `
+    <h3 id="home-header"> Hot reads in your neighborhood 🔥🔥</h2>
+        <div id="container"> </div>
+    <h3>Users Near You</h3>
+        <div class="user-home"> </div>
     `
 }
 
@@ -32,12 +58,12 @@ function renderUser(userObj){
     
 }
 
-function renderBookshelfBook(book, copy) {
+function renderBookshelfBook(copyObj) {
     userBookshelf.innerHTML += `
     <div class="myLibraryBook">
-        <h3 class="title"> ${book.title} </h3>
-        <h4 class="author"> ${book.author} </h4>
-        <h5 class="status"> ${copy.active} </h5>
+        <h3 class="title"> ${copyObj.book.title} </h3>
+        <h4 class="author"> ${copyObj.book.author} </h4>
+        <h5 class="status"> ${copyObj.active} </h5>
     </div>
     `
 }
@@ -48,29 +74,36 @@ function renderNoBorrowings() {
     `
 }
 
-function renderBorrowedBook(book, copy) {
+function renderBorrowedBook(copyObj) {
     userBorrowings.innerHTML += `
     <div class="myLibraryBook">
-        <h3 class="title"> ${book.title} </h3>
-        <h4 class="author"> ${book.author} </h4>
+        <h3 class="title"> ${copyObj.book.title} </h3>
+        <h4 class="author"> ${copyObj.book.author} </h4>
     </div>
     `
 }
 
 function renderBookShow(bookObj) {
-    bookShowContainer.innerHTML += `
-    <div class="book-info">
+    console.log(bookInfoContainer)
+    bookInfoContainer.innerHTML += `
+    
         <h2> ${bookObj.title} </h2>
         <h3> ${bookObj.author} </h3>
         <h4> ${bookObj.genre} </h4>
         <h4> ${bookObj.year} </h4>
         <h4> ${bookObj.rating} </h4>
-    </div>
+    
     `
-    console.log('wass')
     bookObj.book_copies.forEach((copy) => {
         const copyLi = document.createElement("li")
-        li.textContent = copy.condition
+        const owner = document.createElement("p")
+        const rentButton = document.createElement("button")
+        copyLi.textContent = copy.user.name
+        copyLi.dataset.id = copy.user_id
+        owner.textContent = copy.condition
+        rentButton.textContent = "RENT"
+
+        copyLi.append(owner, rentButton)
         bookShowCopies.append(copyLi)
     })
 
@@ -78,10 +111,12 @@ function renderBookShow(bookObj) {
 
 /*********Fetch Requests*********/
 function getBooks() {
+    libraryPage.innerHTML = " "
     fetch('http://localhost:3000/api/v1/books')
     .then(res => res.json())
     .then(bookArray => {
         let i = 0
+        renderHomePage()
         bookArray.forEach((book) => {
             if (i < 8) {
                 renderBook(book)
@@ -108,10 +143,8 @@ function getUsers() {
 function getBookShelf() {
     client.get(`/users/${currentUserId}`)
         .then(userObj => {
-            let i = 0
-            userObj.books.forEach((book) => {
-                renderBookshelfBook(book, userObj.book_copies[i])
-                i++
+            userObj.book_copies.forEach((copyObj) => {
+                renderBookshelfBook(copyObj)
             })
         })
 }
@@ -120,17 +153,14 @@ function getBorrowedBooks() {
     client.get(`/book_copies/`)
         .then(copyArray => {
             let myBorrowedBooks = copyArray.filter(obj => {
-                return obj.renter_id === currentUserIdInt
+                return obj.borrower_id === currentUserIdInt
             })
             if (myBorrowedBooks.length === 0) {
                 renderNoBorrowings()
             } else {
-                console.log('gotta fix this')
-                // client.get(``)
-                // userObj.book_copies.forEach((copy) => {
-                //     renderBorrowedBook(userObj.books[i], copy)
-                //     i++
-                // })
+                myBorrowedBooks.forEach((book) => {
+                    renderBorrowedBook(book)
+                })
             }
         })
 }
@@ -148,10 +178,32 @@ function getBookInfo(id){
 
 /*********Event Listeners*********/
 
+myLibraryButton.addEventListener("click", event => {
+    homePage.innerHTML = " "
+    libraryPage.innerHTML = " "
+    getBookShelf()
+    getBorrowedBooks()
+})
+
+homePageHeaderButton.addEventListener("click", event => {
+    libraryPage.innerHTML = " "
+    homePage.innerHTML = " "
+    getBooks()
+    getUsers()
+})
+
+bookDisplayHome.addEventListener('click', event => {
+    event.preventDefault()
+    if (event.target.matches('div.book-card')) {
+        let id = parseInt(event.target.id)
+        getBookInfo(id)
+    } else if (event.target.closest('div.book-card')) {
+        let id = parseInt(event.target.closest('div.book-card').id)
+        getBookInfo(id)
+    }
+})
 
 /*********Initialize*********/
-// getBooks()
-// getUsers()
-getBookShelf()
-getBorrowedBooks()
+getBooks()
+getUsers()
 // getBookInfo(3)
